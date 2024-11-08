12754ac605ce0894ac865f448e7f48054e0e7919
afzalfaizi <afzaalm993@gmail.com>
2024-11-08 22:37:45 +0500

    update the code

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to Inventory Management System"}
+    return {"message": "Welcome to Mart Inventory Management System"}
 
 
 init_db()
